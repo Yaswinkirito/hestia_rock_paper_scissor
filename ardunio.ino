596c323a230824c9b8a64fcbc96fc8659781ff49
Yaswinkirito <96726310+Yaswinkirito@users.noreply.github.com>
2023-04-27 18:42:15 +0530

    source code

---
+++ ardunio.ino
@@ -0,0 +1,107 @@
+#include <Servo.h>
+
+Servo servothumb;          
+Servo servoindex;          
+Servo servomiddle;
+Servo servoring;
+Servo servopinky;
+
+
+char number[50];
+char c;
+int state = 0;
+String myStringRec;
+int stringCounter = 0;
+bool stringCounterStart = false;
+String myRevivedString;
+int stringLength = 6;
+
+int servoPinky,servoMiddle,servoIndex,servoThumb,servoRing;
+int myVals[] ={0,0,0,0,0} ;
+
+int myButton= 1;
+bool portable;
+
+void setup() {
+
+  Serial.begin(9600);
+  servothumb.attach(9);  
+  servoindex.attach(10);  
+  servopinky.attach(7);
+  servoring.attach(4);
+  servomiddle.attach(3);
+  allOFF();
+
+}
+
+void loop() {
+
+
+receiveData();
+if (servoPinky ==1){  servopinky.write(180);}else{servopinky.write(0);}
+if (servoIndex ==1){  servoindex.write(180);}else{servoindex.write(0);}
+if (servoMiddle ==1){  servomiddle.write(180);}else{servomiddle.write(0);}
+if (servoThumb ==1){  servothumb.write(120);}else{servothumb.write(0);}
+if (servoRing ==1){  servoring.write(180);}else{servoring.write(0);}
+delay(1000);
+
+}
+
+
+void allON(){
+  
+servopinky.write(180);
+servoindex.write(180);
+servomiddle.write(180);
+servothumb.write(120);
+servoring.write(180);
+  
+  }
+
+
+void allOFF(){
+  
+servopinky.write(0);
+servoindex.write(0);
+servomiddle.write(0);
+servothumb.write(0);
+servoring.write(0);
+}
+
+
+void receiveData() {
+  int i = 0;
+  while (Serial.available()) {
+   char c = Serial.read();
+   Serial.print(c);
+  
+    if (c == '$') {
+      stringCounterStart = true;
+    }
+    if (stringCounterStart == true )
+    {
+      if (stringCounter < stringLength)
+      {
+        myRevivedString = String(myRevivedString + c);
+        stringCounter++;
+      }
+      if (stringCounter >= stringLength) {
+        stringCounter = 0; stringCounterStart = false;
+        servoPinky = myRevivedString.substring(1, 2).toInt();
+        servoRing = myRevivedString.substring(2, 3).toInt();
+        servoMiddle = myRevivedString.substring(3, 4).toInt();
+        servoIndex = myRevivedString.substring(4, 5).toInt();
+        servoThumb = myRevivedString.substring(5, 6).toInt();
+      
+//        Serial.print(servoRing);
+//        Serial.print(" ");
+//        Serial.print(servoMiddle);
+//        Serial.print(" ");
+//        Serial.print(servoIndex);
+//        Serial.print(" "); 
+//        Serial.println(servoThumb);       
+        myRevivedString = "";
+      }
+    }
+  }
+}
